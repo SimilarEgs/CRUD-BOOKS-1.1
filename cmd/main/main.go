4fc1191180f99445dfc281cba6bd0a1a4f56ba59
SimilarEgs <newfrontierorder@gmail.com>
2022-06-10 13:55:55 +0300

    redid dbconfig

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -8,13 +8,18 @@ import (
 	"github.com/gorilla/mux"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
 
+	"github.com/SimilarEgs/CRUD-BOOKS-1.1/pkg/dbconfig"
+	"github.com/SimilarEgs/CRUD-BOOKS-1.1/pkg/models"
 	"github.com/SimilarEgs/CRUD-BOOKS-1.1/pkg/routes"
 )
 
 func main() {
 	r := mux.NewRouter()
 	routes.RoutesHandlers(r)
-	fmt.Println("Server started on port - 8080")
 	log.Fatal(http.ListenAndServe("localhost:8080", r))
+	fmt.Println("Server started on port - 8080")
+
+	var db = dbconfig.CreateDbConection()
+	models.InitDB(db)
 
 }
